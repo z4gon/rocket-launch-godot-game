b1f2ca8ee8aedc77dffa087bd00aff3cf6cefc1c
z4gon <4588601+z4gon@users.noreply.github.com>
2023-01-28 20:23:34 -0300

    Texture button and signals

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,7 +1,10 @@
 extends Node
 
 # onready var animationPlayer : AnimationPlayer = get_node("AnimationPlayer")
-onready var animationPlayer : AnimationPlayer = $AnimationPlayer
+onready var animationPlayer : AnimationPlayer = $MyAnimationPlayer
 
 func _ready():
+	pass
+
+func _on_LaunchButton_pressed():
 	animationPlayer.play("Launch")
